7df8138479b34f545db3135684f1b7fef68a0173
kentaro <kentaro@localhost.localdomain>
2017-12-16 12:21:19 +0900

    db作成

--- Dormitory_Laundry/config/routes.rb
+++ Dormitory_Laundry/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	get 	'login'	=> 'sessions#new'
+	post 	'login'	=> 'sessions#create'
+	delete 'logout'	=> 'sessions#destroy' 
+
 end
