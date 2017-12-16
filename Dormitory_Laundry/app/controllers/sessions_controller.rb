d10a1d399bee6ed16961c01acf444aecb67cc6a4
kentaro <kentaro@localhost.localdomain>
2017-12-16 12:37:16 +0900

    login実装

--- Dormitory_Laundry/app/controllers/sessions_controller.rb
+++ Dormitory_Laundry/app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+	user = User.find_by(user_id: params[:session][:user_id])
+	if user && User.find_by(password: params[:session][:password])
+	redirect_to 'https://www.google.co.jp'
+	else
+	redirect_to 'https://www.yahoo.co.jp'
+	end
+  end
 end
